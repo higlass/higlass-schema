34113c8c7e3deabee654017345113442b81fdf5f
Trevor Manz <trevor.j.manz@gmail.com>
2023-03-14 18:03:08 -0400

    feat: add check unit tests

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,2 +1,21 @@
-def test_import_ok():
-    import higlass_schema  # noqa
+import pathlib
+
+import pydantic
+import pytest
+
+import higlass_schema as hgs
+
+fixtures = pathlib.Path(__file__).parent / ".." / "fixtures"
+
+valid = fixtures.glob("**/*.json")
+
+@pytest.mark.parametrize("path", list(valid))
+def test_valid_viewconf(path: pathlib.Path):
+    hgs.Viewconf[hgs.View[hgs.Track]].parse_file(path)
+
+invalid = fixtures.glob("**invalid/*.json")
+
+@pytest.mark.parametrize("path", list(valid))
+def test_invalid_viewconf(path: pathlib.Path):
+    with pytest.raises(pydantic.ValidationError):
+        hgs.Viewconf[hgs.View[hgs.Track]].parse_file(path)
