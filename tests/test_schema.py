79420f4ee19f92038a2ea3411e92c2d13ed3d0a6
Trevor Manz <trevor.j.manz@gmail.com>
2023-03-14 20:26:02 -0400

    fix linting/formatting

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -12,14 +12,15 @@ fixtures = pathlib.Path(__file__).parent / "fixtures"
 def test_valid_viewconf(path: pathlib.Path):
     hgs.Viewconf[hgs.View[hgs.Track]].parse_file(path)
 
+
 @pytest.mark.parametrize("path", fixtures.glob("test/view-configs-invalid/*.json"))
 def test_invalid_viewconf(path: pathlib.Path):
     with pytest.raises(pydantic.ValidationError):
         hgs.Viewconf[hgs.View[hgs.Track]].parse_file(path)
 
+
 @pytest.mark.parametrize("path", fixtures.glob("test/view-configs-tracks/*.json"))
 def test_valid_track(path: pathlib.Path):
-
     # needed to allow testing a union
     class RootTrack(pydantic.BaseModel):
         __root___: hgs.Track
